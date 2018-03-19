62adeefb64222a9aee44fc98700eb8660f276313
Arthur4580 <arthur.harutyunyan@tumo.org>
2018-03-19 15:04:44 +0400

    Lesson_3_4

---
+++ lesson_3/index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var obj={
+    "first_name":"Vardan",
+    "last_name":"Hovsepyan",
+    "age":13,
+    "tumo_student":true
+};
+
+
+
+
+var myJSON = JSON.stringify(obj);
+
+
+
+function main() {
+   fs.writeFileSync("obj.js", myJSON);
+}
+
+main();
